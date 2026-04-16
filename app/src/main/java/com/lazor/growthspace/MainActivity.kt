fad768578e337a44eae36e378992eaac1c38b6e5
Stanislav Lazor <staasuk@gmail.com>
2026-04-16 21:04:35 +0300

    Initial commit.

---
+++ app/src/main/java/com/lazor/growthspace/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.lazor.growthspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lazor.growthspace.ui.theme.GrowthSpaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GrowthSpaceTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GrowthSpaceTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/lazor/growthspace/navigation/Routes.kt
@@ -0,0 +1,8 @@
+package com.lazor.growthspace.navigation
+
+
+object Routes{
+    const val LOGIN = "login"
+    const val REGISTER = "register"
+    const val HOME = "home"
+}
